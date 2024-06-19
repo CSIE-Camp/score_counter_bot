f0e8610cdea2c2a7bc1f168a898290c298c19a40
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-06-19 15:14:16 +0800

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -23,10 +23,43 @@ while 1:
 
     data["embeds"] = [
         {
-            "description": "每30秒更新",
-            "title": "賭場即時排名",
+            "description": "個人排名",
+            "title": "個人排名",
             "color":0xfe7e06,
             "fields":display_
+        }
+    ]
+
+    result = requests.post(url, json=data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+
+    s = []
+    with open("data.json", newline="") as jsonfile:
+        data = json.load(jsonfile)
+
+        for i in data:
+            s.append([data[i]["total"], i])
+        s.sort(key=lambda x: x[0], reverse=True)
+
+    display_ = []
+
+    for i in range(min(10, len(s))):
+        cn_number = "一二三四五六七八九十"
+        display_.append({"name": "第{}名".format(cn_number[i]),
+                         "value": "第{}組 \n 籌碼數量 : {}".format(cn_number[int(s[i][1]) - 1], s[i][0])})
+
+    data["embeds"] = [
+        {
+            "description": "小隊排名",
+            "title": "小隊排名",
+            "color": 0x0000CCFF,
+            "fields": display_
         }
     ]
 
